12240a90343efd188807dd38c90575a1239d8b8c
Santosh Venkatraman <santosh.venk@gmail.com>
2017-06-22 01:27:40 +0530

    Add football api (#1)
    * Add isomorphic-fetch dependency
    
    * Adds Football API's internal api

---
+++ src/api/methods.js
@@ -0,0 +1,110 @@
+import apiRequest from './api-request';
+
+const addQueryParam = (endpoint, queryParam, queryParamValue, isQueryParamAdded=false) {
+  queryParamSeparator = isQueryParamAdded ? '&' : '';
+  return `${endpoint}${queryParamSeparator}?${queryParam}=${queryParamValue}`;
+};
+
+const fetchCompetitions = () => apiRequest('/competitions');
+
+const fetchCompetitionTeams = id =>
+  apiRequest(`/competitions/${id}/teams`);
+
+const fetchCompetitionLeagueTable = (id, matchday='') => {
+  let leagueTableEndpoint = `/competitions/${id}/leagueTable`;
+  if (matchDay) {
+    leagueTableEndpoint = addQueryParam(
+      leagueTableEndpoint,
+      'matchday',
+      matchDay,
+      false
+    );
+  }
+  return apiRequest(leagueTableEndpoint);
+};
+
+const fetchCompetitionFixtures = (id, timeFrame='', leagueCode='') => {
+  let competitionFixturesEndpoint = `/competitions/${id}/fixtures`;
+  let queryParamAdded = false;
+  if (timeFrame) {
+    competitionFixturesEndpoint = addQueryParam(
+        competitionFixturesEndpoint,
+        'timeFrame',
+        timeFrame,
+        queryParamAdded
+    );
+    queryParamAdded = true;
+  }
+  if (leagueCode) {
+    competitionFixturesEndpoint = addQueryParam(
+        competitionFixturesEndpoint,
+        'league',
+        leagueCode,
+        queryParamAdded
+    );
+  }
+  return apiRequest(competitionFixturesEndpoint);
+}
+
+const fetchFixtures = () => apiRequest('/fixtures');
+
+const fetchFixture = (id, headToHead='') => {
+  let fixtureEndpoint = `/fixtures/${id}`;
+  if (headToHead) {
+    fixtureEndpoint = addQueryParam(
+      fixtureEndpoint,
+      'head2head'
+      headToHead
+      false
+    );
+  }
+  return apiRequest(fixtureEndpoint);
+};
+
+const fetchTeamFixtures = (id, season='', timeFrame='', venue='') => {
+  let teamFixturesEndpoint = `/teams/${id}/fixtures`;
+  let queryParamAdded = false;
+  if (season) {
+    teamFixturesEndpoint = addQueryParam(
+      teamFixturesEndpoint,
+      'season'
+      season
+      queryParamAdded
+    );
+    queryParamAdded = true;
+  }
+  if (timeFrame) {
+    teamFixturesEndpoint = addQueryParam(
+      teamFixturesEndpoint,
+      'timeFrame'
+      timeFrame
+      queryParamAdded
+    );
+    queryParamAdded = true;
+  }
+  if (venue) {
+    teamFixturesEndpoint = addQueryParam(
+      teamFixturesEndpoint,
+      'venue'
+      venue
+      queryParamAdded
+    );
+  }
+  return apiRequest(teamFixturesEndpoint);
+};
+
+const fetchTeam = id => apiRequest(`/teams/${id}`);
+
+const fetchTeamPlayers = id => apiRequest(`/teams/${id}/players`);
+
+export {
+  fetchCompetitions,
+  fetchCompetitionTeams,
+  fetchCompetitionLeagueTable,
+  fetchCompetitionFixtures,
+  fetchFixtures,
+  fetchFixture,
+  fetchTeamFixtures,
+  fetchTeam,
+  fetchTeamPlayers
+};
