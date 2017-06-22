08da3a51fab7c244fcdb4e69a2fc1e6fe392b6e3
Santosh Venkatraman <santosh.venk@gmail.com>
2017-06-22 21:36:00 +0530

    Fix api methods (#2)

--- src/api/api-request.js
+++ src/api/api-request.js
@@ -3,9 +3,14 @@ import { get } from './fetch-wrapper';
 
 const generateRequestOptions = endpoint => {
   return {
-    url: `${config.footballAPI.baseURL}${endpoint}`;
+    url: `${config.footballAPI.baseURL}${endpoint}`,
     headers: { 'X-Auth-Token': config.footballAPI.token }
   }
 };
 
-const apiRequest = endpoint => get(generateRequestOptions(endpoint));
+const apiRequest = endpoint =>
+  get(generateRequestOptions(endpoint)).then(response => {
+    return response.statusCode === 200 ? response : Promise.reject(response);
+  });
+
+export default apiRequest;
