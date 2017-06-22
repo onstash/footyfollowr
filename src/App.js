3e0ff2db577c5d7a59815035273ec71119fbb1b4
Santosh Venkatraman <santosh.venk@gmail.com>
2017-06-23 00:48:24 +0530

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Competitions from './components/competitions';
+import Competition from './components/competition';
+import Teams from './components/teams';
+import Fixtures from './components/fixtures';
+import LeagueTable from './components/league-table';
 
 class App extends Component {
   render() {
-    return <Competitions />;
+    return (
+      <Router>
+        <div>
+          <Route exact path="/competitions/:id/league-table" component={ LeagueTable }/>
+          <Route exact path="/competitions/:id/fixtures" component={ Fixtures }/>
+          <Route exact path="/competitions/:id/teams" component={ Teams }/>
+          <Route exact path="/competitions/:id" component={ Competition }/>
+          <Route exact path="/" component={ Competitions }/>
+        </div>
+      </Router>
+    );
     // return (
     //   <div className="App">
     //     <div className="App-header">
