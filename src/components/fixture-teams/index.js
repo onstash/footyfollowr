a23088115a6f87fd3881d04ff30a385908d37e83
Santosh Venkatraman <santosh.venk@gmail.com>
2017-08-18 17:51:42 +0530

    Fixes fixture design issues

--- src/components/fixture-teams/index.js
+++ src/components/fixture-teams/index.js
@@ -8,27 +8,27 @@ const FixtureTeams = ({ homeTeam, awayTeam }) => {
   return (
     <div className="fixture-teams">
       <div className="home-team">
-        <div className="name">
-          { homeTeam }
-        </div>
         <img
           src={teamLogos[homeTeam]}
           alt={homeTeam}
           className="crest"
         />
+        <div className="name">
+          { homeTeam }
+        </div>
       </div>
       <div className="divider">
         vs
       </div>
       <div className="away-team">
-        <div className="name">
-          { awayTeam }
-        </div>
         <img
           src={teamLogos[awayTeam]}
           alt={homeTeam}
           className="crest"
         />
+        <div className="name">
+          { awayTeam }
+        </div>
       </div>
     </div>
   );
