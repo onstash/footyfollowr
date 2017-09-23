2e96d6dfb17f703a411b6361ba46bf13be0d3133
Santosh Venkatraman <santosh.venk@gmail.com>
2017-09-23 13:14:24 +0530

    Removes caching for Competition, Fixtures & LeagueTable (#46)

--- src/data/fixtures.js
+++ src/data/fixtures.js
@@ -1,30 +1,6 @@
 import { fetchCompetitionFixtures as _fetchCompetitionFixtures } from '../api/methods';
 
-import Cache from '../utils/cache';
-
-const fetchCompetitionFixtures = (competitionID, filterParam) => {
-  const key = Cache.generateCacheKey(
-    Cache.keys.FIXTURES, `${competitionID}&${filterParam}`
-  );
-  return Cache
-    .get(key)
-    .then(response => {
-      // console.log('response (cache)', response);
-      _fetchCompetitionFixtures(competitionID, filterParam)
-        .then(apiResponse => Cache.set(key, apiResponse))
-        .catch(error => console.log('error (api)', error));
-      return response;
-    }).catch(error => {
-      // console.log('error (cache)', error);
-      return _fetchCompetitionFixtures(competitionID, filterParam)
-        .then(apiResponse => {
-          // console.log('api response', apiResponse);
-          Cache
-            .set(key, apiResponse)
-            .catch(error => console.log('error (cache)', error));
-          return apiResponse;
-        }).catch(Promise.reject);
-    });
-};
+const fetchCompetitionFixtures = (competitionID, filterParam) =>
+  _fetchCompetitionFixtures(competitionID, filterParam);
 
 export default fetchCompetitionFixtures;
--- src/data/league-table.js
+++ src/data/league-table.js
@@ -2,32 +2,7 @@ import {
   fetchCompetitionLeagueTable as _fetchCompetitionLeagueTable
 } from '../api/methods';
 
-import Cache from '../utils/cache';
-
-const fetchCompetitionLeagueTable = (competitionID, filterParam) => {
-  const key = Cache.generateCacheKey(
-    Cache.keys.LEAGUE_TABLE, `${competitionID}&${filterParam}`
-  );
-  console.log('key', key);
-  return Cache
-    .get(key)
-    .then(response => {
-      console.log('response (cache)', response);
-      _fetchCompetitionLeagueTable(competitionID, filterParam)
-        .then(apiResponse => Cache.set(key, apiResponse))
-        .catch(error => console.log('error (api)', error));
-      return response;
-    }).catch(error => {
-      console.log('error (cache)', error);
-      return _fetchCompetitionLeagueTable(competitionID, filterParam)
-        .then(apiResponse => {
-          console.log('api response', apiResponse);
-          Cache
-            .set(key, apiResponse)
-            .catch(error => console.log('error (cache)', error));
-          return apiResponse;
-        }).catch(Promise.reject);
-    });
-};
+const fetchCompetitionLeagueTable = (competitionID, filterParam) =>
+  _fetchCompetitionLeagueTable(competitionID, filterParam);
 
 export default fetchCompetitionLeagueTable;
