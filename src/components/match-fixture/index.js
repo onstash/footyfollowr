2a58ac919ecbe3a49a53d5acc7653f50bb22b847
Santosh Venkatraman <santosh.venk@gmail.com>
2017-09-28 23:07:53 +0530

    Adds mixpanel tracking for fixture

--- src/components/match-fixture/index.js
+++ src/components/match-fixture/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import DataLayer from '../../data';
+import mixpanel from '../../utils/mixpanel';
+import Cache from '../../utils/cache';
 
 import Fixture from '../fixture';
 import Loader from '../loader';
@@ -19,6 +21,21 @@ class MatchFixture extends React.Component {
     DataLayer.fetchFixture(this.props.match.params.fixtureID).then(({ data: { fixture: _fixture, head2head } }) => {
       const fixture = Object.assign({}, _fixture, { headToHead: head2head });
       this.setState(() => ({ loading: false, fixture }));
+      Cache.get(Cache.keys.MIXPANEL_DISTINCT_ID)
+        .then(distinctID => {
+          const eventProperties = {
+            id: this.props.match.params.fixtureID,
+            homeTeamName: fixture.homeTeamName,
+            awayTeamName: fixture.awayTeamName,
+            scheduled: fixture.date,
+            matchDay: fixture.matchday
+          };
+          mixpanel.track(
+            distinctID,
+            'Fixture Viewed',
+            eventProperties
+          );
+        }).catch(console.error)
     });
   }
 
