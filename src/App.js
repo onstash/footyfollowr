d986a370f2379b65c946539a2c876532ba41069d
Santosh Venkatraman <santosh.venk@gmail.com>
2017-10-02 01:41:21 +0530

    Cleans up App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,5 @@
-// import React, { Component } from 'react';
 import './App.css';
 
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-// import Competitions from './components/competitions';
 import Onboarding from './components/onboarding';
-// import Competition from './components/competition';
-// import Teams from './components/teams';
-// import Fixtures from './components/fixtures';
-// import MatchFixture from './components/match-fixture';
-// import LeagueTable from './components/league-table';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <Router>
-//         <div>
-//           <Route exact path="/fixtures/:fixtureID" component={ MatchFixture }/>
-//           <Route exact path="/competitions/:id/league-table" component={ LeagueTable }/>
-//           <Route exact path="/competitions/:id/fixtures" component={ Fixtures }/>
-//           <Route exact path="/competitions/:id/teams" component={ Teams }/>
-//           <Route exact path="/competitions/:id" component={ Competition }/>
-//           <Route exact path="/competitions" component={ Competitions }/>
-//           <Route exact path="/" component={ Onboarding }/>
-//         </div>
-//       </Router>
-//     );
-//     // return (
-//     //   <div className="App">
-//     //     <div className="App-header">
-//     //       <img src={logo} className="App-logo" alt="logo" />
-//     //       <h2>Welcome to React</h2>
-//     //     </div>
-//     //     <p className="App-intro">
-//     //       To get started, edit <code>src/App.js</code> and save to reload.
-//     //     </p>
-//     //   </div>
-//     // );
-//   }
-// }
 
 export default Onboarding;
