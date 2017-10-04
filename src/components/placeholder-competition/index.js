4db1b745a1cd5c4443761f50c2bf6018779f4b4d
Santosh Venkatraman <santosh.venk@gmail.com>
2017-10-04 07:51:02 +0530

    Cleanup competition card (#66)
    * Removes unnecessary info from Competition card
    
    * Adds text-align: center

--- src/components/placeholder-competition/index.js
+++ src/components/placeholder-competition/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 const PlaceholderCompetition = () => (
   <div className="fa-placeholder-competition-container">
-    <div className="fa-placeholder-competition-caption" />
-    <div className="fa-placeholder-competition-games" />
     <div className="fa-placeholder-competition-match-day" />
   </div>
 );
