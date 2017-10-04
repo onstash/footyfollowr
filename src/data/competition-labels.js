e2b0974397844dcd7f7a5527ca5d184054f76192
Santosh Venkatraman <santosh.venk@gmail.com>
2017-10-04 07:33:03 +0530

    Cleans up scrollable tap size (#65)

---
+++ src/data/competition-labels.js
@@ -0,0 +1,19 @@
+export default {
+  "Premier League 2017/18": "English Premier League",
+  "Campeonato Brasileiro da Série A": "Brazilian Serie A",
+  "Championship 2017/18": "English Championship",
+  "League One 2017/18": "English League One",
+  "League Two 2017/18": "English League Two",
+  "Eredivisie 2017/18": "Dutch Premier League",
+  "Ligue 1 2017/18": "French Ligue 1",
+  "Ligue 2 2017/18": "French Ligue 2",
+  "1. Bundesliga 2017/18": "German Bundesliga 1",
+  "2. Bundesliga 2017/18": "German Bundesliga 2",
+  "Primera Division 2017": "Spanish La Liga",
+  "Serie A 2017/18": "Italian Serie A",
+  "Primeira Liga 2017/18": "Portuguese Primeira Liga",
+  "DFB-Pokal 2017/18": "German DFB-Pokal",
+  "Serie B 2017/18": "Italian Serie B",
+  "Champions League 2017/18": "Champions League",
+  "Australian A-League": "Australian League A"
+}
