375dd41f5074ec49fdeaadb8c4c2f03ed09f6d45
Santosh Venkatraman <santosh.venk@gmail.com>
2017-10-09 12:28:07 +0530

    Sorts leagues by rankings & removes "Australian A-League"

---
+++ src/data/league-labels.js
@@ -0,0 +1,18 @@
+export default {
+  "Premier League 2017/18": "English Premier League",
+  "Primera Division 2017": "Spanish La Liga",
+  "Champions League 2017/18": "UEFA Champions League",
+  "1. Bundesliga 2017/18": "German Bundesliga 1",
+  "Serie A 2017/18": "Italian Serie A",
+  "Ligue 1 2017/18": "French Ligue 1",
+  "Primeira Liga 2017/18": "Portuguese Primeira Liga",
+  "Eredivisie 2017/18": "Dutch Premier League",
+  "DFB-Pokal 2017/18": "German DFB-Pokal",
+  "2. Bundesliga 2017/18": "German Bundesliga 2",
+  "Serie B 2017/18": "Italian Serie B",
+  "Ligue 2 2017/18": "French Ligue 2",
+  "League One 2017/18": "English League One",
+  "Championship 2017/18": "English Championship",
+  "League Two 2017/18": "English League Two",
+  "Campeonato Brasileiro da Série A": "Brazilian Serie A",
+};
---
+++ src/data/league-rankings.js
@@ -0,0 +1,19 @@
+export default {
+  "Premier League 2017/18": 1,
+  "Primera Division 2017": 2,
+  "Champions League 2017/18": 3,
+  "1. Bundesliga 2017/18": 4,
+  "Serie A 2017/18": 5,
+  "Ligue 1 2017/18": 6,
+  "Primeira Liga 2017/18": 7,
+  "Eredivisie 2017/18": 8,
+  "DFB-Pokal 2017/18": 9,
+  "2. Bundesliga 2017/18": 10,
+  "Serie B 2017/18": 11,
+  "Ligue 2 2017/18": 12,
+  "League One 2017/18": 13,
+  "Championship 2017/18": 14,
+  "League Two 2017/18": 15,
+  "Campeonato Brasileiro da Série A": 16,
+  "Australian A-League": 17
+};
