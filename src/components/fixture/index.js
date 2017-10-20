21d5175b0acdff755bd4141e7636d88341dbd7a1
Santosh Venkatraman <santosh.venk@gmail.com>
2017-10-21 04:59:18 +0530

    Adds condition to render FixtureSubscription component
    The fixture should be SCHEDULED & cannot be live at the time.

--- src/components/fixture/index.js
+++ src/components/fixture/index.js
@@ -5,6 +5,8 @@ import FixtureResult from '../fixture-result';
 import FixtureTeams from '../fixture-teams';
 import FixtureSubscription from '../fixture-subscription';
 
+const Dummy = () => <div />;
+
 const Fixture = ({
   homeTeamName,
   awayTeamName,
@@ -17,12 +19,19 @@ const Fixture = ({
   const timeDifferenceInMins = (new Date() - new Date(date)) / (1000 * 60);
   const isGameLive = timeDifferenceInMins > 0 && timeDifferenceInMins < 115;
   const className = isGameLive ? 'fa-fixture-container-live' : 'fa-fixture-container';
+  const showNotificationsToggle = isGameLive === false && status === 'SCHEDULED';
+  const NotificationToggle = showNotificationsToggle ? FixtureSubscription : Dummy;
   return (
     <div className={className}>
       <FixtureTeams homeTeam={homeTeamName} awayTeam={awayTeamName} />
       <FixtureResult {...result} timeDifferenceInMins={timeDifferenceInMins} />
       <FixtureDate date={date} status={status} />
-      <FixtureSubscription fixtureID={fixtureID} date={date} />
+      <NotificationToggle
+        fixtureID={fixtureID}
+        date={date}
+        homeTeamName={homeTeamName}
+        awayTeamName={awayTeamName}
+      />
     </div>
   );
 };
