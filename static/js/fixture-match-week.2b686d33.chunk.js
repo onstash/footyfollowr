ed74fc1c834256c1cc7dedfe6570e838a9075dd1
Santosh Venkatraman <santosh.venk@gmail.com>
2017-11-12 13:25:52 +0530

    Updates

---
+++ static/js/fixture-match-week.2b686d33.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([12],{92:function(e,a,t){"use strict";Object.defineProperty(a,"__esModule",{value:!0});var r=t(2),c=function(e){var a=e.number;return r.a.createElement("div",{className:"fa-fixture-match-week"},r.a.createElement("div",{className:"fa-fixture-match-week-label"},"Week"),r.a.createElement("div",{className:"fa-fixture-match-week-number"},a))};a.default=c}});
+//# sourceMappingURL=fixture-match-week.2b686d33.chunk.js.map
