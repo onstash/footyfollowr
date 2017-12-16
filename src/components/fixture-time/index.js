0846df9842ce9db5e8261aa4f8c11d97babac53e
Santosh Venkatraman <santosh.venk@gmail.com>
2017-12-17 00:09:49 +0530

    Fixes fixture date labels (today, yesterday, tomorrow etc) (#95)

--- src/components/fixture-time/index.js
+++ src/components/fixture-time/index.js
@@ -10,15 +10,17 @@ class FixtureTime extends React.Component {
 
   componentWillMount() {
     const { fixtureTime, currentTime } = this.props;
-    const dayDifference = Math.abs(currentTime.getDate() - fixtureTime.getDate());
-    if (dayDifference > 1) {
+    const dayDifference = currentTime.getDate() - fixtureTime.getDate();
+    if (dayDifference === 1) {
+      this.setState(() => ({ time: `Yesterday at ${generateDateString(fixtureTime, true)}` }));
+    } else if (dayDifference === 0) {
+      this.setState(() => ({ time: `Today at ${generateDateString(fixtureTime, true)}` }));
+    } else if (dayDifference === -1) {
+      this.setState(() => ({ time: `Tomorrow at ${generateDateString(fixtureTime, true)}` }));
+    } else {
       this.setState(() => ({
         time: generateDateString(fixtureTime)
       }))
-    } else if (dayDifference === 0) {
-      this.setState(() => ({ time: `Today at ${generateDateString(fixtureTime, true)}` }));
-    } else if (dayDifference === 1) {
-      this.setState(() => ({ time: `Tomorrow at ${generateDateString(fixtureTime, true)}` }));
     }
   }
 
