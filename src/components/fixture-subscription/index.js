19cc2e6cd5b6e7837e84c2cdbc051d342dd3f40a
Santosh Venkatraman <santosh.venk@gmail.com>
2018-06-15 23:13:20 +0530

    Adds mixpanel tracking for notifications
    * Notifications permissions
    * Notification toggle on / off
    * Notification received

--- src/components/fixture-subscription/index.js
+++ src/components/fixture-subscription/index.js
@@ -4,26 +4,24 @@ import { subscribeToNotifications, checkSubscription } from '../../api/methods';
 import messaging from '../../utils/firebase';
 import Cache from '../../utils/cache';
 
-const firebaseMessaging = messaging();
+import mixpanel from '../../utils/mixpanel';
 
-firebaseMessaging.onMessage(payload => {
-  if (payload.notification) {
-    alert(`${payload.notification.title}`);
-  }
-});
+const firebaseMessaging = messaging();
 
 const Toggled = () => (
   <img
     className="fa-fixture-subscription-icon"
     alt="notifications-button-toggled"
-    src={require('./notifications-button-toggled.png')} />
+    src={require('./notifications-button-toggled.png')}
+  />
 );
 
 const Untoggled = () => (
   <img
     className="fa-fixture-subscription-icon"
     alt="notifications-button-untoggled"
-    src={require('./notifications-button.png')} />
+    src={require('./notifications-button.png')}
+  />
 );
 
 const ToggleLoading = () => (
@@ -32,18 +30,78 @@ const ToggleLoading = () => (
   </div>
 );
 
-const Dummy = () => <div />;
+const Dummy = () => null;
+
+const fetchDistinctIDFromCache = () =>
+    Cache.get(Cache.keys.MIXPANEL_DISTINCT_ID)
+        .then(distinctID => distinctID, error => {
+            console.log({ source: "fetchDistinctIDFromCache", error });
+            return Promise.reject(error);
+        });
+
+
+const trackNotificationToggle = ({ toggleState, home_team: homeTeam, away_team: awayTeam, date, fixture_id: fixtureID }) =>
+    fetchDistinctIDFromCache().then(distinctID =>
+        mixpanel.track(
+            distinctID,
+            "Notification Bell Toggled",
+            { value: toggleState, homeTeam, awayTeam, date, fixtureID }
+        )
+    );
+
+
+const trackNotificationDelivered = ({ appState, title, userConfirmation }) =>
+    fetchDistinctIDFromCache().then(distinctID =>
+        mixpanel.track(
+            distinctID,
+            "Push Notification Received",
+            { appState, title, userConfirmation }
+        )
+    );
+
+
+const trackNotificationPermissionResponse = ({ userResponse }) =>
+    fetchDistinctIDFromCache().then(distinctID =>
+        mixpanel.track(
+            distinctID,
+            "Push Notification Permission",
+            { granted: userResponse }
+        )
+    );
+
+
+const fetchNotificationsPermissionFromUser = () =>
+    firebaseMessaging.requestPermission()
+        .then(() => {
+            trackNotificationPermissionResponse({ userResponse: true });
+            return;
+        }, error => {
+            trackNotificationPermissionResponse({ userResponse: false });
+            return Promise.reject(error);
+        });
+
+const fetchNotificationsPermissionFromCache = ({ fetchPermission = false }) =>
+    Cache.get(Cache.keys.NOTIFICATIONS_PERMISSIONS_REQUESTED)
+        .then(fcmToken => fcmToken, error => {
+            if (fetchPermission) {
+                return fetchNotificationsPermissionFromUser();
+            }
+            return Promise.reject(error);
+        });
+
 
 class FixtureSubscription extends React.Component {
   constructor() {
     super();
     this.state = { hasSubscribed: false, loading: true };
     this._onClick = this.onClick.bind(this);
+    this._onNotificationMessage = this._onNotificationMessage.bind(this);
   }
 
   componentDidMount() {
-    Cache.get(Cache.keys.NOTIFICATIONS_PERMISSIONS_REQUESTED)
-      .then(() => firebaseMessaging.requestPermission())
+    firebaseMessaging.onMessage(this._onNotificationMessage);
+
+    fetchNotificationsPermissionFromCache({ fetchPermission: false })
       .then(() => firebaseMessaging.getToken())
       .then(fcmToken => {
         const { fixtureID } = this.props;
@@ -54,27 +112,33 @@ class FixtureSubscription extends React.Component {
         this.setState(() => ({ hasSubscribed, loading: false }));
       })
       .catch(error => {
-        console.log({ source: "FixtureSubscription", error });
         this.setState(() => ({ loading: false }));
       });
   }
 
+  _onNotificationMessage(payload) {
+    if (payload.notification) {
+        const title = payload.notification.title;
+        const userConfirmation = confirm(`${title}`); // eslint-disable-line no-restricted-globals
+        trackNotificationDelivered({ appState: "foreground", title, userConfirmation });
+    }
+  }
+
   onClick() {
-    firebaseMessaging.requestPermission()
-      .then(() => {
-        return firebaseMessaging.getToken();
-      })
+    const { fixtureID, homeTeamName, awayTeamName, date } = this.props;
+    const fixtureData = {
+        fixture_id: fixtureID,
+        home_team: homeTeamName,
+        away_team: awayTeamName,
+        date
+    };
+    fetchNotificationsPermissionFromCache({ fetchPermission: true })
+      .then(() => firebaseMessaging.getToken())
       .then(fcmToken => {
         this.setState(() => ({ loading: true }));
-        const { fixtureID, homeTeamName, awayTeamName, date } = this.props;
         const payload = {
           fcm_token: fcmToken,
-          fixture_data: {
-            fixture_id: fixtureID,
-            home_team: homeTeamName,
-            away_team: awayTeamName,
-            date
-          }
+          fixture_data: fixtureData
         };
         return subscribeToNotifications(payload);
       })
@@ -82,8 +146,10 @@ class FixtureSubscription extends React.Component {
         if (message === "Fixture subscribed successfully") {
           Cache.set(Cache.keys.NOTIFICATIONS_PERMISSIONS_REQUESTED, true);
           this.setState(() => ({ hasSubscribed: true, loading: false }));
+          trackNotificationToggle({ toggleState: true, ...fixtureData });
         } else if (message === "Fixture unsubscribed successfully") {
           this.setState(() => ({ hasSubscribed: false, loading: false }));
+          trackNotificationToggle({ toggleState: false, ...fixtureData });
         }
       })
       .catch(error => {
